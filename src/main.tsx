4900494f0a35c230489c778d2974f6ca847795c6
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2025-05-22 16:07:42 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+import "./styles/index.scss";
+
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
         <App />
